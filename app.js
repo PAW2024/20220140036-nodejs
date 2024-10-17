89bf876da6ddea75261cf2a147eccfae5ff5b5aa
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-17 15:52:19 +0700

    menambahkan todo post

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express'); // Mengimport express
 const app = express();
 const port = 3000;
+const todoroutes = require('./routes/todo.js');
 
+app.use(express.json());
 
+app.use('/todos',todoroutes);
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
